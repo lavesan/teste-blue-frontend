f1a4a3da113b9115fee5c61b3191ff1c70e405e7
Valdery Alves Paes Junior <valdery.jur@gmail.com>
2020-05-17 21:41:26 -0300

    :sparkles: Configures libraries
    - Sets vuelidate globally
    - Sets vue-toastify globally
    - Sets base URL to axios globally
    
    Signed-off-by: Valdery Alves Paes Junior <valdery.jur@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,15 @@
 <script>
 import { RegiterUserComponent } from './components/register-user';
 import axios from 'axios';
+import Vuelidate from 'vuelidate';
+import Vue from 'vue';
+import VueToast from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-default.css';
 
-axios.defaults.baseURL = environment.API_URL;
+axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+
+Vue.use(Vuelidate);
+Vue.use(VueToast, { position: 'top' });
 
 export default {
   name: 'App',
@@ -18,6 +25,14 @@ export default {
 }
 </script>
 
-<style>
-#app {}
+<style lang="scss">
+  @import "@coreui/coreui/scss/coreui.scss";
+
+  body {
+    min-height: 100vh;
+  }
+
+  #app {
+    min-height: 100vh;
+  }
 </style>

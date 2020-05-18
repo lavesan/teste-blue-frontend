aeba52d81d0c258227549d7bd5fd95394cf6186f
Valdery Alves Paes Junior <valdery.jur@gmail.com>
2020-05-17 21:42:55 -0300

    Sets styles, field validations and sends the data to the endpoint
    Signed-off-by: Valdery Alves Paes Junior <valdery.jur@gmail.com>

--- src/components/register-user/register-user.code.js
+++ src/components/register-user/register-user.code.js
@@ -1,17 +1,24 @@
 import userInstance from '../../services/user.service';
-import { validationMixin } from "vuelidate";
 import {
     required,
     email,
-    email,
   } from "vuelidate/lib/validators";
 import { characterRegex, phoneRegex } from '../../helpers/validations.helpers';
-import { CInput } from '@coreui/coreui';
+import { phoneMask } from '../../helpers/mask.helpers';
+import { CInput, CForm, CContainer, CCol, CRow, CButton } from '@coreui/vue';
 
 export default {
-    mixins: [validationMixin],
+    name: 'RegisterUser',
     components: {
+        CCol,
+        CRow,
+        CForm,
         CInput,
+        CButton,
+        CContainer,
+    },
+    mounted() {
+        this.$v.form.$touch();
     },
     data() {
         return {
@@ -21,24 +28,84 @@ export default {
                 email: '',
                 phone: '',
             },
+            loading: false,
         }
     },
     validations: {
-        name: {
-            required,
-            validateName(name) {
-              return characterRegex.test(name);
+        form: {
+            name: {
+                required,
+                validateName(name) {
+                  return characterRegex.test(name);
+                },
+            },
+            email: {
+                required,
+                email,
+            },
+            phone: {
+                required,
+                validatePhone(phone) {
+                    return phoneRegex.test(phone);
+                },
             },
         },
-        email: {
-            required,
-            email,
+    },
+    methods: {
+        onInputChange(value, name) {
+
+            if (name === 'phone') {
+                this.form[name] = phoneMask(value);
+            } else {
+                this.form[name] = value;
+            }
+
         },
-        phone: {
-            required,
-            validatePhone(phone) {
-                return phoneRegex.test(phone);
-            },
+        clearForm() {
+
+            const entries = Object.keys(this.form);
+            entries.forEach(key => {
+                this.form[key] = '';
+            });
+
+        },
+        async saveUser() {
+
+            this.loading = true;
+            if (this.$v.form.$pending || this.$v.form.$error) {
+                this.loading = false;
+                return;
+            }
+
+            const onlyNumberPhone = this.form.phone.replace(/\D/g, '');
+            const ddd = onlyNumberPhone.match(/^\d{2}/)[0];
+            const phoneNumber = onlyNumberPhone.match(/\d{9}$/)[0];
+
+            const body = {
+                Email: this.form.email,
+                Name: this.form.name,
+                Ddd: ddd,
+                PhoneNumber: phoneNumber,
+            }
+
+            await this.userService.save(body)
+                .then(() => {
+                    this.clearForm();
+                    this.$toast.open({
+                        type: 'success',
+                        message: 'Seus dados foram salvos com sucesso!',
+                    });
+                })
+                .catch(() => {
+                    this.$toast.open({
+                        type: 'error',
+                        message: 'Infelizmente aconteceu um erro interno, por favor tente mais tarde :/',
+                    });
+                    console.log('caiu no err');
+                });
+
+            this.loading = false;
+
         },
     },
 }
--- src/components/register-user/register-user.component.vue
+++ src/components/register-user/register-user.component.vue
@@ -1,6 +1,45 @@
 <template>
-    <div>
-        <CInput />
+    <div class="d-flex justify-content-center align-items-center h-100 register-user-page">
+        <CForm @submit.prevent="saveUser">
+            <h1 class="h3 mb-4">Cadastre-se!</h1>
+            <fieldset>
+                <label for="email-form">Email</label>
+                <CInput
+                    id="email-form"
+                    type="email"
+                    @input="value => onInputChange(value, 'email')"
+                    :value="form.email"
+                    :is-valid="!$v.form.email.$error" />
+            </fieldset>
+            <fieldset>
+                <label for="name-form">Nome</label>
+                <CInput
+                    id="name-form"
+                    @input="value => onInputChange(value, 'name')"
+                    :value="form.name"
+                    :is-valid="!$v.form.name.$error" />
+            </fieldset>
+            <fieldset>
+                <label for="phone-form">Telefone</label>
+                <CInput
+                    id="phone-form"
+                    type="tel"
+                    @input="value => onInputChange(value, 'phone')"
+                    :value="form.phone"
+                    maxlength="15"
+                    :is-valid="!$v.form.phone.$error" />
+            </fieldset>
+            <div class="d-flex justify-content-end">
+                <CButton
+                    color="success"
+                    type="submit">
+                    Salvar
+                </CButton>
+            </div>
+        </CForm>
     </div>
 </template>
 <script src="./register-user.code"></script>
+<style lang="scss">
+@import "./register-user.styles";
+</style>
